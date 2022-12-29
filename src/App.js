c93a2d34e3ddfae94c8cfbb4bbb39b4c4595c594
Waqar Hussain Ujjan <94596486+waqarhussain786@users.noreply.github.com>
2022-12-29 14:18:02 +0500

    creating navigation bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!!!</h1>
+    <div className="navigation">
+      <ul>
+        <li className="list">
+          <a href="#">
+            <span className="icon"></span>
+            <span className="icon">Home</span>
+          </a>
+          <a href="#">
+            <span className="icon"></span>
+            <span className="icon">Profile</span>
+          </a>
+          <a href="#">
+            <span className="icon"></span>
+            <span className="icon">Message</span>
+          </a>
+          <a href="#">
+            <span className="icon"></span>
+            <span className="icon">Setting</span>
+          </a>
+          <a href="#">
+            <span className="icon"></span>
+            <span className="icon">Photos</span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
-
 export default App;
