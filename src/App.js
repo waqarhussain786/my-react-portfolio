96d801f8b0a88939b2e4bcf0521dad753bacea26
Waqar Hussain Ujjan <94596486+waqarhussain786@users.noreply.github.com>
2022-12-29 15:04:44 +0500

    navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import './App.css';
+import './app.css';
+import { HiOutlineHome } from 'react-icons/hi';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineMessage } from 'react-icons/ai';
+import { FiSettings } from 'react-icons/fi';
+import { MdOutlinePhotoCamera } from 'react-icons/md';
 
 function App() {
   return (
@@ -6,23 +11,23 @@ function App() {
       <ul>
         <li className="list">
           <a href="#">
-            <span className="icon"></span>
+            <span className="icon" ><HiOutlineHome /></span>
             <span className="icon">Home</span>
           </a>
           <a href="#">
-            <span className="icon"></span>
+            <span className="icon"><CgProfile /></span>
             <span className="icon">Profile</span>
           </a>
           <a href="#">
-            <span className="icon"></span>
+            <span className="icon"><AiOutlineMessage /></span>
             <span className="icon">Message</span>
           </a>
           <a href="#">
-            <span className="icon"></span>
+            <span className="icon"><FiSettings /></span>
             <span className="icon">Setting</span>
           </a>
           <a href="#">
-            <span className="icon"></span>
+            <span className="icon"><MdOutlinePhotoCamera /></span>
             <span className="icon">Photos</span>
           </a>
         </li>
