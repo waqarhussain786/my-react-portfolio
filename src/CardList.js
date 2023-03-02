9969846015b64a0da0ee47bcef5820ac8863c9b4
waqarhussain786 <wh751212@gmail.com>
2023-03-03 00:21:42 +0500

    responsive

--- src/CardList.js
+++ src/CardList.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Card from './Card';
+import './home.css'
 
 const CardList = ({ RobotList }) => {
 
     return (
-        <div>
+        <div className='center'>
             {RobotList.map((user, i) => {
                 return <Card key={i} id={RobotList[i].id} name={RobotList[i].Name} email={RobotList[i].email} />
             })}
