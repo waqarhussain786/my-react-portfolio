b2280699777a4d36541f3f8b4ee3878209bc4058
waqarhussain786 <wh751212@gmail.com>
2023-03-02 19:28:42 +0500

    robotic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
+import Cards from "./Cards";
+import { cards_data } from "./cards_object";
 
-class App extends Component {
-    render(){
-        return<h1>hello world</h1>
-    }
+
+const App = () => {
+    return (
+        <>
+            <Cards id={cards_data[0].id} userName={cards_data[0].userName} email={cards_data[0].email} />
+            <Cards id={cards_data[1].id} userName={cards_data[1].userName} email={cards_data[1].email} />
+            <Cards id={cards_data[2].id} userName={cards_data[2].userName} email={cards_data[2].email} />
+            <Cards id={cards_data[3].id} userName={cards_data[3].userName} email={cards_data[3].email} />
+            <Cards id={cards_data[4].id} userName={cards_data[4].userName} email={cards_data[4].email} />
+        </>
+    );
 }
 
 export default App;
---
+++ src/cards_object.jsx
@@ -0,0 +1,27 @@
+export const cards_data = [
+{
+    id: 0,
+    userName: 'waqar0',
+    email: 'wh751212@gmail.com'
+},    
+{
+    id: 1,
+    userName: 'waqar1',
+    email: 'wh751212@gmail.com'
+},    
+{
+    id: 2,
+    userName: 'waqar2',
+    email: 'wh751212@gmail.com'
+},    
+{
+    id: 3,
+    userName: 'waqar3',
+    email: 'wh751212@gmail.com'
+},    
+{
+    id: 4,
+    userName: 'waqar4',
+    email: 'wh751212@gmail.com'
+}    
+];
