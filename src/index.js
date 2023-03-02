81022d4f4de00ffd8c6792e152930ebaf0fa826d
waqarhussain786 <wh751212@gmail.com>
2023-03-02 17:42:19 +0500

    react-udemy-course

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Navbartop from './navbar';
-import NavbarBootstrap from './navbarBootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavbarBootstrap />
-    {/* <Navbartop /> */}
     <App />
   </React.StrictMode>
 );
