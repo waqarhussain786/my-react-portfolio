647e3ce5647f819aaa6d873309de41a1808ddc75
waqarhussain786 <wh751212@gmail.com>
2023-03-31 18:50:15 +0500

    m

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
     return (
         <div className='tc'>
             <h1>RoboFriends</h1>
+            
             <SearchBox />
             <CardList RobotList={RobotList} />
         </div>
